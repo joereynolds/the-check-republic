4c0125e2951adab1ce298e6b6818e0e11527b3a6
Joe Reynolds <joereynolds952@gmail.com>
2021-04-19 22:03:47 +0100

    Clean up main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,35 +1,19 @@
 #!/usr/bin/env lua
 local lfs = require "lfs"
-local config = require "./src/config"
-local notifier = require "./src/notifiers/notify-send"
+local check = require "./src/check"
 
-
-local path_separator = package.config:sub(1,1)
-local check_directory = os.getenv("HOME")
-    .. path_separator ..".config"
-    .. path_separator .. "check-republic"
-    .. path_separator .. "checks"
-
-local check_directory_attributes = lfs.attributes(check_directory)
+local check_directory_attributes = lfs.attributes(check.directory)
 
 if check_directory_attributes == nil then
     print("No check directory found.")
-    print("I couldn't find " .. check_directory)
+    print("I couldn't find " .. check.directory)
     print("Do this:")
-    print("mkdir -p " .. check_directory)
+    print("mkdir -p " .. check.directory)
     os.exit(false)
 end
 
-for file in lfs.dir(check_directory) do
+for file in lfs.dir(check.directory) do
     if file ~= '.' and file ~= '..' then
-        local config_file = check_directory .. path_separator .. file .. path_separator .. "config.lua"
-        local check_file = check_directory .. path_separator .. file .. path_separator .. "check"
-        local check_config = config.get(config_file)
-
-        if not check_config["disabled"] then
-            if not os.execute(check_file) then
-                os.execute(notifier.get_notify_command(check_config["name"], check_config["message"]))
-            end
-        end
+        check.run(file)
     end
 end
