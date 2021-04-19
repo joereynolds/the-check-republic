1657dec26984fa88d9f3aad0250371b377730601
Joe Reynolds <joereynolds952@gmail.com>
2021-04-19 11:38:40 +0100

    Add basic install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+# sudo apt install lua5.3
+# sudo apt install liblua5.3-dev
+sudo luarocks install luafilesystem
+sudo luarocks install luaunit
+sudo luarocks install luastatic
+sudo luarocks install luacheck
+
