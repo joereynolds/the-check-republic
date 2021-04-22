8fa03ff8855c0b064dc539a31ed8aab9bd2f32d5
Joe Reynolds <joereynolds952@gmail.com>
2021-04-22 20:18:54 +0100

    Update exe script

--- build/create-executable.sh
+++ build/create-executable.sh
@@ -1,3 +1,6 @@
+rm -rf dist/
+mkdir dist
+
 cd src
 
 luastatic main.lua check.lua config.lua util.lua notifiers/notify-send.lua /usr/lib/x86_64-linux-gnu/liblua5.3.a -I/usr/include/lua5.3
@@ -5,5 +8,6 @@ luastatic main.lua check.lua config.lua util.lua notifiers/notify-send.lua /usr/
 commit_id=$(git rev-parse --short HEAD)
 
 rm main.luastatic.c
-mv main ../check-republic-$commit_id
+mv main ../dist/check-republicd-$commit_id
+
 cd -
