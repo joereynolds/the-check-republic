d3f2d59e14b3f4373c9ff46cb3c1172d791f9677
Joe Reynolds <joereynolds952@gmail.com>
2021-04-22 13:43:12 +0100

    Move to busted for tests (it's better)

---
+++ spec/config_spec.lua
@@ -0,0 +1,27 @@
+local config = require "../src/config"
+
+describe("The config module", function()
+    it("has a config which falls back to defaults if no file is found", function()
+        local expected = {
+            name = "A check has failed",
+            rate = "5m",
+            message = "Check your system",
+            disabled = false
+        }
+
+        local actual = config.get()
+        assert.are.same(expected, actual)
+    end)
+
+    it("has a sensible default for the 'name' parameter", function()
+        local expected = "./spec/empty-config.lua"
+        local actual = config.get("./spec/empty-config.lua")["name"]
+        assert.are.same(expected, actual)
+    end)
+
+    it("has a sensible default for the 'message' parameter", function()
+        local expected = "This check has failed."
+        local actual = config.get("./spec/empty-config.lua")["message"]
+        assert.are.same(expected, actual)
+    end)
+end)
