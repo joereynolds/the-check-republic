f84bf580b0fbce28d4977f3511dbb5bb93ee7155
Joe Reynolds <joereynolds952@gmail.com>
2021-04-29 10:30:31 +0100

    Write out service file via lua

--- spec/service_spec.lua
+++ spec/service_spec.lua
@@ -13,4 +13,27 @@ describe("The check module", function()
         local actual = service.make_option('Environment', 'DISPLAY=:0')
         assert.are.equal(expected, actual)
     end)
+
+    it("creates a service file given some parameters", function()
+        local expected = [[
+[Unit]
+Description="Check Republic Service"
+[Service]
+User="joe"
+Type="oneshot"
+Environment="DISPLAY=:0"
+Environment="XAUTH=/bla/bla"
+Environment="DBUS=/blabla"
+ExecStart="/home/joe/bin/check-republicd"]]
+
+        local actual = service.make_service_file(
+            "joe",
+            "DISPLAY=:0",
+            "XAUTH=/bla/bla",
+            "DBUS=/blabla",
+            "/home/joe/bin/check-republicd"
+        )
+
+        assert.are.equal(expected, actual)
+    end)
 end)
--- src/service.lua
+++ src/service.lua
@@ -1,9 +1,6 @@
 -- Writes out some environment variables
 -- that are required for the .service
 -- files to work
-
-
-
 local M = {}
 
 function M.get_display()
@@ -20,5 +17,26 @@ end
 function M.make_option(key, value)
     return key .. '=' .. '"' .. value .. '"'
 end
+
+function M.make_service_file(user, display, xauthority, dbus, exec_start)
+    local service_file = "[Unit]" .. "\n"
+
+    service_file = service_file .. M.make_option("Description", "Check Republic Service") .. "\n"
+
+    service_file = service_file .. "[Service]" .. "\n"
+    service_file = service_file .. M.make_option("User", user) .. "\n"
+    service_file = service_file .. M.make_option("Type", "oneshot") .. "\n"
+    service_file = service_file .. M.make_option("Environment", display) .. "\n"
+    service_file = service_file .. M.make_option("Environment", xauthority) .. "\n"
+    service_file = service_file .. M.make_option("Environment", dbus) .. "\n"
+    service_file = service_file .. M.make_option("ExecStart", exec_start)
+
+    return service_file
+end
+
+-- function M.write_service_file(user, display, xauthority, dbus, exec_start)
+    -- local service_file = M.make_service_file(user, display, xauthority, dbus, exec_start)
+    -- ...
+-- end
 
 return M
