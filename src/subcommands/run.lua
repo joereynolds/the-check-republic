eb276a7ac0273e34f9c6bda31091f7e9cb61f144
Joe Reynolds <joereynolds952@gmail.com>
2021-04-29 16:32:12 +0100

    Move commands into subcommands
    This will give the user a cleaner interface when using check-republic.
    The idea being they download a single binary and then run:
    ```
    check-republic install
    ```
    
    This command will generate a service file, move it over to /etc/systemd/
    and also enable it with systemctl.
    
    Once installation has been done, the service file will call the same
    binary with the `run` subcommand instead.

---
+++ src/subcommands/run.lua
@@ -0,0 +1,24 @@
+local lfs = require "lfs"
+local check = require "check"
+
+local M = {}
+
+function M.process()
+    local check_directory_attributes = lfs.attributes(check.directory)
+
+    if check_directory_attributes == nil then
+        print("No check directory found.")
+        print("I couldn't find " .. check.directory)
+        print("Do this:")
+        print("mkdir -p " .. check.directory)
+        os.exit(false)
+    end
+
+    for file in lfs.dir(check.directory) do
+        if file ~= '.' and file ~= '..' then
+            check.run(file)
+        end
+    end
+end
+
+return M
---
+++ src/subcommands/subcommands.lua
@@ -0,0 +1,5 @@
+return {
+    run = require "subcommands/run",
+    help = require "subcommands/help",
+    install = require "subcommands/install",
+}
